3718f77f929431956fc83edde4075986659a4157
1922015 <1922015@users.noreply.github.com>
2017-06-03 12:25:22 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,11 +21,10 @@ function draw() {
   verde=random(0,255)
   azul=random(0,255)
 
-  background(255,0,0);
+  //background(255,0,0);
   //fill(100,200,100)
-  fill(rojo, verde, azul, 0);
+  fill(rojo, verde, azul);
+  //fill(rojo, verde, azul, 0);
   rect(400,250,200,200);
   ellipse(mouseX,mouseY,250,250);
   //x=x+2;
-
-}
